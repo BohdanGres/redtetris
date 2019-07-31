ddac4b9e8de98d447b2a261c3180cf777ddc0ab6
Oleksandra Pokusynska <opokusyn@student.unit.ua>
2019-07-31 20:33:22 +0300

    tests

--- test/init.js
+++ test/init.js
@@ -1,10 +1,15 @@
-import * as actions from '../src/client/actions/init'
+import * as actions from '../src/client/actions/error'
+import * as action from '../src/client/actions/init' //init
 import {expect, any} from 'chai'
 
 
 describe('user test all actions', () => {
   it('should test init function', () => {
-      expect(actions.init()).to.deep.equal({type:'INIT_TYPE', body: any})
+      expect(action.init()).to.deep.equal({type:'INIT_TYPE', body: any})
+    }
+  )
+  it('should test error function', () => {
+      expect(actions.error()).to.deep.equal({type:'ERROR', error: any})
     }
   )
 })
